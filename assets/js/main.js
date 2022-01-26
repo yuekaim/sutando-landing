f8cc43789636ed546ae86ce6125a43c13bf5b5c3
yuekaim <yuekaim@gmail.com>
2022-01-26 19:14:34 +0100

    temp styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
-$('.landing-bg').mousemove(function( event ) {
-  alert('hello');
-  var x = event.pageX;
-  var y = event.pageY;
-  $('.landing-bg').css('transform', 'skew(50deg)');
-  $('.landing-bg').css('width', '150vw');
-});
+// $('.landing-bg').mousemove(function( event ) {
+//   var x = event.pageX;
+//   var y = event.pageY;
+//   $('.landing-bg').css('transform', 'skew(50deg)');
+//   $('.landing-bg').css('width', '150vw');
+// });
